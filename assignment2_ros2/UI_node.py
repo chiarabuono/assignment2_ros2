e8dd0a9415766f0cf6361c1a0b5496a795768dca
chiarabuono <s7687956@studenti.unige.it>
2025-01-04 13:59:39 +0000

    Robot moving in the environment

--- assignment2_ros2/UI_node.py
+++ assignment2_ros2/UI_node.py
@@ -5,15 +5,41 @@ from rclpy.node import Node
 from rclpy.exceptions import ParameterNotDeclaredException
 from rcl_interfaces.msg import ParameterType
 from std_msgs.msg import String
-
-# import rclpy.node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 
 class RobotController(Node):
 
     def __init__(self):
         super().__init__('robot_controller')
-               
-       
+
+        # publisher that publishes the velocity of the robot /cmd_vel topic    
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.timer_move = self.create_timer(0.1, self.move_robot)
+
+        # subscriber that subscribes to the odom message of the odom topic
+        self.subscriber_ = self.create_subscription(Odometry, '/odom', self.listener, 10)
+
+    def move_robot(self):
+        velocityChosen = Twist()
+        x_pos = self.odom.pose.pose.position.x 
+        # y_pos = self.odom.pose.pose.position.y
+
+        angular_z = 0.0
+        linear_x = 1.0
+        
+        if x_pos > 9.0: angular_z = 1.0
+        elif x_pos < 2.0: angular_z = -1.0
+
+        velocityChosen.linear.x = linear_x
+        velocityChosen.angular.z = angular_z      
+
+        self.publisher_.publish(velocityChosen)
+
+
+    def listener(self, msg):
+        self.odom = msg
+        
 
 # one node capable of moving the robot around in the simulation environment
 def main(args=None):
@@ -22,10 +48,13 @@ def main(args=None):
     rclpy.init(args=args)
     robot_controller = RobotController()
 
-    
-    rclpy.spin(robot_controller)
-    robot_controller.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(robot_controller)
+    except KeyboardInterrupt:
+        print("\nExiting.")
+    finally:
+        robot_controller.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'UI_node = assignment2_ros2.UI_node:main'
         ],
     },
 )
